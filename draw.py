54eea6e7c9c89d1bc4d8f31225a77e34e90e1e35
Piotr Sikorski <piotrek.sikorski@hotmail.com>
2021-11-28 03:52:17 +0100

    int mapping

--- draw.py
+++ draw.py
@@ -32,10 +32,15 @@ def draw(input_data: list):
     """
     robot_xs, robot_ys = [], []
     left_xs, left_ys = [], []
+    f_left_xs, f_left_ys = [], []
     middle_xs, middle_ys = [], []
     right_xs, right_ys = [], []
     input_data = input_data[1:]
+    i = 0
     for measure in input_data:
+        i += 1
+        if i > 88:
+            break
         robot_x, robot_y, robot_angle, left, middle, right = [float(x) for x in measure]
         
         #for robot_x, robot_y, robot_angle, left, middle, right in measure:
@@ -52,18 +57,21 @@ def draw(input_data: list):
 
         if middle < 200:
             middle_xs.append(robot_x +
-                                get_x_part(left, robot_angle + MIDDLE_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
+                                get_x_part(middle, robot_angle + MIDDLE_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
             middle_ys.append(robot_y +
-                                get_y_part(left, robot_angle + MIDDLE_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
+                                get_y_part(middle, robot_angle + MIDDLE_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
         if right < 200:
             right_xs.append(robot_x +
-                            get_x_part(left, robot_angle + RIGHT_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
+                            get_x_part(right, robot_angle + RIGHT_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
             right_ys.append(robot_y +
-                            get_y_part(left, robot_angle + RIGHT_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
-        
-            
+                            get_y_part(right, robot_angle + RIGHT_DISTANCE_SENSOR_ANGLE_FROM_ROBOT_CENTRE))
+
+    for i in range(6, len(left_xs)):
+        f_left_xs.append((left_ys[i-6]+left_ys[i-5]+left_xs[i-4]+left_xs[i-3]+left_xs[i-2]+left_xs[i-1]+left_xs[i])/7)
+        f_left_ys.append((left_ys[i-6]+left_ys[i-5]+left_ys[i-4]+left_ys[i-3]+left_ys[i-2]+left_ys[i-1]+left_ys[i])/7)
     plt.plot(robot_xs, robot_ys, 'r.')
     plt.plot(left_xs, left_ys, 'blue', linewidth=0.1, marker='.')
+    plt.plot(f_left_xs, f_left_ys, 'green', linewidth=0.1, marker='.')
     plt.plot(middle_xs, middle_ys, 'cyan', linewidth=0.1, marker='.')
     plt.plot(right_xs, right_ys, 'purple', linewidth=0.1, marker='.')
     plt.show()
