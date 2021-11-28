017244d0eeb55611b0a477e5d709c5b251ef4a0a
Piotr Sikorski <piotrek.sikorski@hotmail.com>
2021-11-28 20:32:27 +0100

    draw changes

--- draw.py
+++ draw.py
@@ -71,8 +71,10 @@ def draw(input_data: list):
     for i in range(6, len(left_xs)):
         f_left_xs.append((left_ys[i-6]+left_ys[i-5]+left_xs[i-4]+left_xs[i-3]+left_xs[i-2]+left_xs[i-1]+left_xs[i])/7)
         f_left_ys.append((left_ys[i-6]+left_ys[i-5]+left_ys[i-4]+left_ys[i-3]+left_ys[i-2]+left_ys[i-1]+left_ys[i])/7)
+    for i in range(6, len(middle_xs)):
         f_middle_xs.append((middle_ys[i-6]+middle_ys[i-5]+middle_xs[i-4]+middle_xs[i-3]+middle_xs[i-2]+middle_xs[i-1]+middle_xs[i])/7)
         f_middle_ys.append((middle_ys[i-6]+middle_ys[i-5]+middle_ys[i-4]+middle_ys[i-3]+middle_ys[i-2]+middle_ys[i-1]+middle_ys[i])/7)
+    for i in range(6, len(right_xs)):
         f_right_xs.append((right_ys[i-6]+right_ys[i-5]+right_xs[i-4]+right_xs[i-3]+right_xs[i-2]+right_xs[i-1]+right_xs[i])/7)
         f_right_ys.append((right_ys[i-6]+right_ys[i-5]+right_ys[i-4]+right_ys[i-3]+right_ys[i-2]+right_ys[i-1]+right_ys[i])/7)
     plt.plot(robot_xs, robot_ys, 'r.')
